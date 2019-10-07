3bef24be458d7c1f417c6ed5417ab9280bd3861d
Pontus Kristofer Wadsjö <pokwa@eg.se>
2019-10-07 13:50:07 +0200

    Demo of ICourseInterface

---
+++ DataInterface/ICourseManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface
+{
+    public interface ICourseManager
+    {
+        public void AddCourse(string name);
+    }
+}
