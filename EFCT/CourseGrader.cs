838bbef31fca80cf0d2ffc00d6852740695d331b
Pontus Kristofer Wadsjö <pokwa@eg.se>
2019-10-17 10:58:05 +0200

    CourseGrader and CourseGraderTests now with comments

--- EFCT/CourseGrader.cs
+++ EFCT/CourseGrader.cs
@@ -8,12 +8,22 @@ namespace EFCT
 {
     public class CourseGrader
     {
+        // För att vi ska kunna mocka våra managers skapar vi variabler
+        // för dessa, så att vi kan sätta dem en gång, istället för 
+        // att i koden göra t.ex. "gradeManager = new GradeManager();"
         IGradeManager gradeManager;
         ICourseManager courseManager;
         ITestResultManager testResultManager;
 
+        // Här skickar vi in våra managers när klassen skapas. De ligger
+        // i konstruktorn dels för att vi bara ska behöva skicka in dem
+        // en gång, och dels för att detta garanterar att man inte kan 
+        // glömma att sätta dessa.
         public CourseGrader(IGradeManager gradeManager, ICourseManager courseManager, ITestResultManager testResultManager)
         {
+            // Här har vi ett fält och en parameter som har samma namn
+            // för att sätta fältet refererar vi därför till this. för att
+            // skilja dem åt
             this.gradeManager = gradeManager;
             this.courseManager = courseManager;
             this.testResultManager = testResultManager;
@@ -21,20 +31,36 @@ namespace EFCT
 
         public void GradeCourse(string courseName)
         {
+            // Vi ska sätta betyg på alla elever i en kurs, så vi hämtar
+            // alla elever i kursen och loopar igenom dem
             var students = courseManager.GetStudentsInCourse(courseName);
             foreach(var student in students)
             {
+                // Sen kollar vi på alla prov som studenten har skrivit
+                // i den kursen
                 var examGrades = testResultManager.GetAllTestResultsForOneStudentAndOneCourse(courseName, student.Name);
+                // Vi ska summera resultatet från alla proven, så vi börjar
+                // med en summa på noll, sen loopar vi igenom alla proven
                 decimal sum = 0;
                 foreach(var examGrade in examGrades)
                 {
+                    // För varje prov lägger vi till elevens resultat
+                    // multiplicerat med det provets andel av totalen
                     sum += examGrade.Score * examGrade.ExamAnswer.Exam.FractionOfGrade;
                 }
+                // Sen hämtar vi betygsgränserna
+                // Vi vill ha det minsta resultatet som krävs för att
+                // få ett visst betyg i GradeLimit.Limit, och motsvarande
+                // betyg i GradeLetter. Vi tar därför och väljer alla betyg
+                // som är mindre än eller lika med elevens resultat, och väljer
+                // det högsta genom att sortera dem i fallande ordning och
+                // ta det första
                 var gradeLimits = gradeManager.GetGradeLimits(courseName);
                 string gradeLetter = gradeLimits.Where(
                     gl => gl.Limit <= sum)
                     .OrderByDescending(gl => gl.Limit)
                     .First().GradeLetter;
+                // Till slut sätter vi elevens betyg
                 gradeManager.SetGrade(courseName, gradeLetter, student.Name);
             }
         }
--- UnitTests/CourseGraderTests.cs
+++ UnitTests/CourseGraderTests.cs
@@ -14,14 +14,44 @@ namespace UnitTests
         [TestMethod]
         public void GradeOneStudentOneTest0Percent()
         {
+            // Detta test ska testa att sätta betyg på
+            // en student, som har gått en kurs, med ett
+            // prov, där studenten har noll rätt
+
+            // Här skapar vi upp våra mock-objekt
+            // vi behöver dessa till konstruktorn av vår
+            // CourseGrader. Vi mockar dessa då vi vill
+            // kunna skicka in det testdata vi behöver för 
+            // just det här testet, och för att vi ska slippa
+            // prata med databasen, som är långsam
             var gradeManager = new Mock<IGradeManager>();
             var courseManager = new Mock<ICourseManager>();
             var testResultManager = new Mock<ITestResultManager>();
 
+            // CourseGrader kommer att anropa GetStudentsInCourse,
+            // som tar en sträng som är namnet på kursen som 
+            // parameter, och returnerar en lista av studenter i
+            // kursen.
+            // It.IsAny<string>() betyder att det spelar ingen
+            // roll vilken sträng som CourseGrader kommer att 
+            // skicka in, och .Returns betyder att den metoden
+            // då komer att returnera argumentet till .Returns,
+            // i det här fallet en ny lista som innehåller en 
+            // student som vi inte har satt någon information på.
             courseManager.Setup(cm =>
                 cm.GetStudentsInCourse(It.IsAny<string>()))
                 .Returns(new List<Student> { new Student()});
 
+            // Detta gör samma sak som ovan, men med lite mer 
+            // data. Vi vill ha tillbaka en lista med TestResult:s
+            // I listan lägger vi till en TestResult, och på den
+            // sätter vi en ny ExamAnswer, och på den en ny Exam.
+            // Detta behöver vi för att det i CourseGrader står:
+            // sum += examGrade.Score * examGrade.ExamAnswer.Exam.FractionOfGrade;
+            // Detta innebär att vi läser ExamAnswer från TestResult 
+            // och Exam från ExamAnswer, så om dessa inte är satta
+            // kommer de att vara null när vi kör testet, och
+            // vi kommer att få ett fel.
             testResultManager.Setup(trm =>
                 trm.GetAllTestResultsForOneStudentAndOneCourse(
                     It.IsAny<string>(), It.IsAny<string>()))
@@ -35,6 +65,9 @@ namespace UnitTests
                         }
                     }
                 });
+
+            // Så ger vi GetGradeLimits på vårt mockobjekt
+            // lite data att skicka tillbaka när den anropas
             gradeManager.Setup(gm =>
                 gm.GetGradeLimits(It.IsAny<string>()))
                 .Returns(new List<GradeLimit>
@@ -46,8 +79,19 @@ namespace UnitTests
                     }
                 });
 
+            // Nu skapar vi en CourseGrader, som är det objektet vi ska
+            // testa. Vi skickar in .Object från våra mockobjekt, som 
+            // är mockar som implementerar respektive interface. Det är
+            // på detta sätt vi "styr bort" CourseGrader från databasen
+            // och får den att använda våra mockobjekt istället
             var courseGrader = new CourseGrader(gradeManager.Object, courseManager.Object, testResultManager.Object);
+            // Sen anropar vi metoden som vi vill testa
             courseGrader.GradeCourse("C#");
+            // .Verify på ett mockobjekt motsvarar en Assert.
+            // Här vill vi kontrollera att SetGrade har anropats en 
+            // gång, med den första parametern satt till "C#", den
+            // andra till "F", och den tredje bryr vi oss inte om 
+            // vad den var satt till.
             gradeManager.Verify(
                 gm => gm.SetGrade(
                     It.Is<string>(s => s == "C#"), 
